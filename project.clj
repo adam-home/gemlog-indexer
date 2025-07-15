0e3c872394d27bd34b47363ffb2c4a6563bb39a7
Adam Turnbull <adam@adamt.com>
2025-07-15 21:18:09 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gemlog-indexer "0.1.1"
+(defproject gemlog-indexer "0.1.2"
   :description "Simple atom.xml generator for gemlogs"
-  :url "http://example.com/FIXME"
+  :url "http://spikydinosaur.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure   "1.12.1"]
